09f27bd2cb1f976cb87c3c8a5d214f7bdc65e582
yubin <yb3143@gmail.com>
2024-11-19 17:30:07 +0900

    chore: firebase 설치

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZM9QFnJsEONuRG-L4UBMd_xgu3MIxir8",
+  authDomain: "nibuy-3627b.firebaseapp.com",
+  projectId: "nibuy-3627b",
+  storageBucket: "nibuy-3627b.firebasestorage.app",
+  messagingSenderId: "271949567273",
+  appId: "1:271949567273:web:d67b2ba10789726e865338",
+  measurementId: "G-2YL2XZMDLX",
+};
+
+const app = initializeApp(firebaseConfig);
